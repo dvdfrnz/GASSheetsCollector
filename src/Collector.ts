871091dcbf306fb10c065d894f0f02c8161cf0c1
dvdfrnz <davidfranz@live.com>
2023-04-09 00:37:50 +0200

    added Looker function

--- src/Collector.ts
+++ src/Collector.ts
@@ -1,14 +1,17 @@
 /**
+ * Collector function that returns data range from Spreadsheet
+ * @param {string} spreadSheetId - ID of Spreadsheet File
+ * @param {string} sheetName - Name of the Sheet in Spreadsheet
  *
- * @param folderId
- * @returns
- */
+ * */
 
-const looker = (folderId: string): any[] => {
-  let spreadsheetIds: any[] = [];
+const collector = (spreadSheetId: string, sheetName: string): any[] => {
   try {
-    let data = SpreadsheetApp.openById(folderId);
-    spreadsheetIds.push(data);
+    let data = SpreadsheetApp.openById(spreadSheetId)
+      .getSheetByName(sheetName)
+      .getDataRange()
+      .getValues();
+    return data;
   } catch (error) {
     return [JSON.stringify(error)];
   }
